9b74706c31703f64b3707cbba2fc7bfa4ff55c3f
MADavison <33644117+MADavison@users.noreply.github.com>
2018-11-19 16:47:46 -0700

    Sort added to Update

--- GeneratePanel.java
+++ GeneratePanel.java
@@ -161,6 +161,7 @@ import java.text.*;
 		    	  	//fix paths
 		    	  	intPanel.clearPaths();
 		    	  	intPanel.createPaths(nodelist);
+				intPanel.sortPaths();
 		    	  	
 		    	  	actName.setText(null);
 		    	  	durName.setText(null);
